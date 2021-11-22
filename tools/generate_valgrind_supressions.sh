ae4039b53914726887ee8b884ef9e5a941545c4a
florian <f.m.eigentler@gmail.com>
2021-11-22 12:11:23 +0100

    Minor update.

--- tools/generate_valgrind_supressions.sh
+++ tools/generate_valgrind_supressions.sh
@@ -8,7 +8,7 @@ valgrind \
     --log-file=mpi_supp_scal.log \
     tests/unit_test_MPI_mpi
 
-cat mpi_supp_scal.log | ../tools/parse_valgrind_suppressions.sh > mpi_supp_scal.sup
+cat mpi_supp_scal.log | ../tools/parse_valgrind_suppressions.sh > basec.sup
 
 valgrind \
     --leak-check=full \
@@ -20,4 +20,4 @@ valgrind \
     --log-file=global_supp_scal.log \
     tests/unit_test_Global_global
 
-cat global_supp_scal.log | ../tools/parse_valgrind_suppressions.sh >> mpi_supp_scal.sup
+cat global_supp_scal.log | ../tools/parse_valgrind_suppressions.sh >> basec.sup
